c51bd4061d150da47ae3910d41abbbe291a13e00
HazemFRE <hfrehat105@gmail.com>
2025-02-09 23:13:59 +0300

    hazem

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -39,19 +39,19 @@ Select AVG (Cast (PublishedYear As Float ) ) As AveragePublicationYear FROM Book
   PhoneNumber varchar (30);
 
   UPDATE Authors
-SET Email = 'Ralph@gmail.com', PhoneNumber = '+777777777'
+SET Email = 'Ralph@gmail.com', PhoneNumber = '+54655447'
 WHERE AuthorID = 1;
 
 UPDATE Authors
-SET Email = 'James@gmail.com', PhoneNumber = '+888888888'
+SET Email = 'James@gmail.com', PhoneNumber = '+86456455'
 WHERE AuthorID = 2;
 
 UPDATE Authors
-SET Email = 'James@gmail.com', PhoneNumber = '+999999999'
+SET Email = 'James@gmail.com', PhoneNumber = '+99654599'
 WHERE AuthorID = 3;
 
 UPDATE Authors
-SET Email = 'Frank@gmail.com', PhoneNumber = '+66666666'
+SET Email = 'Frank@gmail.com', PhoneNumber = '+66654516'
 WHERE AuthorID = 4;
 
 UPDATE Authors
